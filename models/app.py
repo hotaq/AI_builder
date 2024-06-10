b978c93d189dffda2ec10d17b9362745275d6354
hotaq <131946401+hotaq@users.noreply.github.com>
2024-06-10 22:02:24 +0700

    Update app.py

--- models/app.py
+++ models/app.py
@@ -46,7 +46,7 @@ def main():
         confidence = float(st.sidebar.slider(
         "select Model Confidence",20,100,30
         ))/100
-        model_path = './main/models/Yolov8.pt'
+        model_path = './Yolov8.pt'
         model = YOLO(model_path)
         model_info_yolov8 = model.info()
         
